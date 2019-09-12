0c9ed7e682e03d9680092a5f9448b69c89841f09
tvdboom <32366550+tvdboom@users.noreply.github.com>
2019-09-12 14:06:57 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+'''
+Automated Tool for Optimized Modelling (ATOM)
+Author: tvdboom
+
+'''
+
+from setuptools import setup
+
+setup(name='ATOM',
+      version='1.0.2',
+      description='AutoML',
+      url='http://github.com/tvdboom/ATOM',
+      author='tvdboom',
+      author_email='m.524687@gmail.com',
+      license='MIT',
+      packages=['ATOM'],
+      zip_safe=False)
