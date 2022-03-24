afab55c7d5ae49909de122a7b0940a04dd38afb8
Mavs <32366550+tvdboom@users.noreply.github.com>
2022-03-24 14:40:14 +0100

    debug gpu import

--- atom/basetransformer.py
+++ atom/basetransformer.py
@@ -442,6 +442,7 @@ class BaseTransformer:
             else:
                 holdout = None
 
+            print(splitter)
             train, test = splitter(
                 data,
                 test_size=test_size,
